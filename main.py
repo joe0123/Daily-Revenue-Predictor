c7ae21d40d5874b0d52474c47d8590282ffb1ce6
b06902125 <b06902125@meow2.csie.ntu.edu.tw>
2020-12-17 15:53:38 +0800

    is canceled model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 from datasets import raw_dataset
 
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     print((1 - model.score(valid_x, valid_y)) * ((valid_y - valid_y.mean()) ** 2).mean())
     
     train_x, train_y, valid_x, valid_y, test_x = dataset.get_cancel_data()
-    model = LogisticRegression(max_iter=1e+5).fit(train_x, train_y)
+    model = LogisticRegression().fit(train_x, train_y)
     print((1 - train_y).mean())
     print(model.score(train_x, train_y))
     print((1 - valid_y).mean())
